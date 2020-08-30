c297ed2849efeb5aef9d6a622c51611eea47107d
dianakakoma <dikakoma@gmail.com>
2020-08-29 22:10:00 -0500

    wrote out RSpec tests for the Report model and create action for the users controller

---
+++ spec/models/report_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Report, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    it 'report can be created' do
+      report = Report.create(name:"test name", description:"test description", suggested_fix:"test suggested fix" url:"test url", status:"test status", screenshot:"test screenshot")
+
+      expect(report).to be_valid
+    end
+  end
+end
