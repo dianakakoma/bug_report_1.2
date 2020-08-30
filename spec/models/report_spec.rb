c281cff501af8b9ebbb6ec46ce9b67e089fe057c
dianakakoma <dikakoma@gmail.com>
2020-08-29 22:49:34 -0500

    corrected typos in the rspec create actions

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Report, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
   describe 'creation' do
     it 'report can be created' do
-      report = Report.create(name:"test name", description:"test description", suggested_fix:"test suggested fix" url:"test url", status:"test status", screenshot:"test screenshot")
+      report = Report.create(name:"test name", description:"test description", suggested_fix:"test suggested fix", url:"test url", status:"test status", screenshot:"test screenshot")
 
       expect(report).to be_valid
     end
--- spec/requests/api/reports_request_spec.rb
+++ spec/requests/api/reports_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::Reports", type: :request do
   describe 'create' do
     it "creates a new bug or idea report" do
-      report = Report.create(name:"test name", description:"test description", suggested_fix:"test suggested fix" url:"test url", status:"test status", screenshot:"test screenshot")
+      report = Report.create(name:"test name", description:"test description", suggested_fix:"test suggested fix", url:"test url", status:"test status", screenshot:"test screenshot")
       expect(Report.last.name).to eq("test name")
     end
   end
